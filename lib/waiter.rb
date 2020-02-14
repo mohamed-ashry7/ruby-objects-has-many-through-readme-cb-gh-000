b862289bb91ff7d38b0c18b40bf479a60c6e7c7f
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-14 12:02:32 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,14 @@
 class Waiter
 
+  attr_accessor :name ,:yrs_experience :
+  @@all = []
+  def initialize(name , years )
+    @name = name
+    @yrs_experience = years
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  
 end
